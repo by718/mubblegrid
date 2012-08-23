a492e8756e21d1c622e6ee8bf4e90662c2e5cb47
Tom Cheng <tcheng@Tom-Chengs-MacBook-Pro.local>
2012-08-22 20:41:01 -0400

    Added code for launching camera.

--- Resources/ScanWindow.js
+++ Resources/ScanWindow.js
@@ -5,6 +5,31 @@ function ScanWindow() {
 		tabBarHidden: 'true'
 	});
 	
+	var cameraButton = Titanium.UI.createButton({title: 'Take Picture'});
+	cameraButton.addEventListener('click', opencamera); 
+	
+	function opencamera() { 
+		Titanium.Media.showCamera({
+			success:function(event){
+				var image = event.media;
+	 			imageView.image = image;
+	        },
+	        cancel:function(){},
+	            error:function(error){
+	                var a = Titanium.UI.createAlertDialog({title:'Camera'});
+	 				if (error.code == Titanium.Media.NO_CAMERA){
+	                    a.setMessage('Your device does not seem to have a camera.');
+	                }
+	                else{
+	                    a.setMessage('Unexpected error: ' + error.code);
+	                }
+	                a.show();
+	            },
+	            saveToPhotoGallery:true,
+	            allowImageEditing:true
+		})};
+	win.add(cameraButton)
+	
     var btnNav = Ti.UI.createButton({
     	title: 'Use',
     	color: '#000',
